ef2657361f1ea33dffdc061ea8af9ad0967119b7
Krzysztof Kobrenczuk <Krzysztof_Kobrenczuk@epam.com>
2020-01-30 16:27:15 +0100

    Finish Backend

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 // All Authors Route
 router.get('/', async (req, res) => {
@@ -40,8 +41,17 @@ router.post('/', async (req, res) => {
 	}
 })
 
-router.get('/:id', (req, res) => {
-	res.send('Show Author ' + req.params.id)
+router.get('/:id', async (req, res) => {
+	try {
+		const author = await Author.findById(req.params.id)
+		const books = await Book.find({author: author.id}).limit(6).exec()
+		res.render('authors/show', {
+			author: author,
+			booksByAuthor: books
+		})
+	} catch {
+		res.redirect('/')
+	}
 })
 
 router.get('/:id/edit', async (req, res) => {
